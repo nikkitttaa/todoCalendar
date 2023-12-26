dee6b9753b9461e808efbcee83fff0681eb55e43
nikittaaa <mogilatnika098@gmail.com>
2023-12-26 16:18:58 +0300

    Календарь с функцие добавления записей в бд

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:todo_calendar/home.dart';
+import 'firebase_options.dart';
+
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MaterialApp(home: HomeScreen()));
+}
